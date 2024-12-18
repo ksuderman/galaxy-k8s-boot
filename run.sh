b808b3987e03e1182c083b405fb998100dc24945
Keith Suderman <suderman@jhu.edu>
2024-12-18 16:41:45 -0600

    Add --break-system-packages

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ansible-playbook -i inventories/hosts.ini -e kube_cloud_provider=openstack -e k8s_provider=rke -c local playbook.yml
+ansible-playbook -i inventories/hosts.ini -e kube_cloud_provider=openstack -e k8s_provider=rke -c local -e --break-system-packages playbook.yml
