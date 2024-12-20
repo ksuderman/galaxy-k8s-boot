9c13852f7d1215f8180f636448ca64912f708ea5
Enis Afgan <afgane@gmail.com>
2024-12-20 11:20:52 -0500

    Use mounted hostpath instead, removing CSIs

--- bin/init_script.sh
+++ bin/init_script.sh
@@ -3,7 +3,6 @@
 # Ensure the script runs with root privileges
 exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&1
 
-VOLUME_ID=vol-09e8d020c214c9d33
 RESERVED_CORES=2
 RESERVED_MEM_MB=6144
 
@@ -17,4 +16,4 @@ cd galaxy-k8s-boot
 
 sed -i "s|extra_server_args=\"--tls-san localhost --disable traefik --v=4\"|extra_server_args=\"--tls-san $(curl -s http://169.254.169.254/latest/meta-data/public-ipv4) --disable traefik --v=4\"|" inventories/localhost
 
-ansible-playbook -i inventories/localhost playbook.yml --extra-vars "kube_cloud_provider=aws" --extra-vars "ebs_volume_id=$VOLUME_ID" --extra-vars "job_max_cores=$(($(nproc) - $RESERVED_CORES))" --extra-vars "job_max_mem=$(( $(free -m | awk '/^Mem:/{print $2}') - $RESERVED_MEM_MB ))"
+ansible-playbook -i inventories/localhost playbook.yml --extra-vars "kube_cloud_provider=aws" --extra-vars "job_max_cores=$(($(nproc) - $RESERVED_CORES))" --extra-vars "job_max_mem=$(($(free -m | awk '/^Mem:/{print $2}') - $RESERVED_MEM_MB))"
