4fe605ad8bca6d98b680f3797290dd94b7264257
Keith Suderman <suderman@jhu.edu>
2024-12-19 20:37:16 -0600

    Modify run.sh to select k3s or rke

--- run.sh
+++ run.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
-ansible-playbook -i inventories/hosts.ini -e kube_cloud_provider=openstack -e k8s_provider=rke -c local playbook.yml
+if [[ $# > 0 ]] ; then
+  case $1 in
+    k3s:
+      playbook=playbook-k3s.yml
+      ;;
+    rke:
+      playbook=playbook-rke.yml
+      ;;
+    -h|--help)
+      echo "USAGE: $(basename $0) [k3s|rke]"
+      exit 0
+      ;;
+    *)
+      echo "ERROR: Invalid option $1"
+      exit 1
+      ;;
+  esac
+else
+  playbook=playbook-rke.yml
+fi
+ansible-playbook -i inventories/hosts.ini -e kube_cloud_provider=openstack -e k8s_provider=rke -c local $playbook
+
