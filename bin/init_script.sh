f3b5193af20653631b41deedb22e375ec29cebb8
nuwang <2070605+nuwang@users.noreply.github.com>
2024-12-20 21:00:04 +0530

    Add support for pulsar

--- bin/init_script.sh
+++ bin/init_script.sh
@@ -3,6 +3,12 @@
 # Ensure the script runs with root privileges
 exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&1
 
+#-------------------------------
+# CloudMC configuration
+#-------------------------------
+# Application is either galaxy or pulsar
+APPLICATION=galaxy
+#-------------------------------
 RESERVED_CORES=2
 RESERVED_MEM_MB=6144
 
@@ -16,4 +22,4 @@ cd galaxy-k8s-boot
 
 sed -i "s|extra_server_args=\"--tls-san localhost --disable traefik --v=4\"|extra_server_args=\"--tls-san $(curl -s http://169.254.169.254/latest/meta-data/public-ipv4) --disable traefik --v=4\"|" inventories/localhost
 
-ansible-playbook -i inventories/localhost playbook.yml --extra-vars "kube_cloud_provider=aws" --extra-vars "job_max_cores=$(($(nproc) - $RESERVED_CORES))" --extra-vars "job_max_mem=$(($(free -m | awk '/^Mem:/{print $2}') - $RESERVED_MEM_MB))"
+ansible-playbook -i inventories/localhost playbook.yml --extra-vars "kube_cloud_provider=aws" --extra-vars "job_max_cores=$(($(nproc) - $RESERVED_CORES))" --extra-vars "job_max_mem=$(($(free -m | awk '/^Mem:/{print $2}') - $RESERVED_MEM_MB))" --extra-vars "application=$APPLICATION"
