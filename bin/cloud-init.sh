affe2f8ee9e4ebc5cfdfa0c6451c9391cad7d91a
Keith Suderman <suderman@jhu.edu>
2024-12-28 20:28:37 -0600

    Cleanup and parameters git repo and branch

--- bin/cloud-init.sh
+++ bin/cloud-init.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-#pwd > /home/ubuntu/cloud-init.start
-#whoami >> /home/ubuntu/cloud-init.start
+OWNER=ksuderman
+NAME=galaxy-k8s-boot
+REPO=${REPO:-https://github.com/$OWNER/$NAME.git}
+BRANCH=${BRANCH:-1-dockerize-rke}
+
 cd /home/ubuntu
-git clone https://github.com/afgane/galaxy-k8s-boot --branch 1-dockerize-rke
-cd galaxy-k8s-boot
+git clone $REPO --branch $BRANCH
+cd $NAME
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 cd ../
-chown -R ubuntu:ubuntu galaxy-k8s-boot
-#touch /home/ubuntu/cloud-init.end
+chown -R ubuntu:ubuntu $NAME
 
