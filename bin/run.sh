43afcbae0180eecfee34fe2d7b7c9e9e9f984e83
Keith Suderman <suderman@jhu.edu>
2025-01-07 13:42:54 -0600

    Testing on gcp

--- bin/run.sh
+++ bin/run.sh
@@ -8,14 +8,34 @@ DIR=/opt/galaxy-k8s-boot
 
 VOLUMES="-v ./.kube:$DIR/.kube -v ./outputs:$DIR/outputs" # -v ./inventories:$DIR/inventories"
 
+while [[ $# > 0 ]] ; do
+  case $1 in
+    --gcp) CLOUD=gcp ;;
+    --aws) CLOUD=aws ;;
+    --openstack) CLOUD=openstack ;;
+    --k3s) K8S=k3s ;;
+    --rk3) K8S=rke ;;
+    --version) VERSION=$2 ; shift ;;
+    *)
+      echo "ERROR: Invalid option $1"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+#docker run --privileged \
+#       --network=host   --cgroupns=host  \
+#       -v /etc/systemd/system:/etc/systemd/system   \
+#       -v /sys/fs/cgroup:/sys/fs/cgroup:ro   \
+#       -v /run/systemd:/run/systemd   \
+#       -v /var/run/dbus:/var/run/dbus   \
+#       -e container=docker \
+#       -e kube_cloud_provider=$CLOUD \
+#       -e k8s_provider=$K8S  \
+#       ksuderman/galaxy-k8s-boot:$VERSION
+
 docker run --privileged \
-       --network=host   --cgroupns=host  \
-       -v /etc/systemd/system:/etc/systemd/system   \
-       -v /sys/fs/cgroup:/sys/fs/cgroup:ro   \
-       -v /run/systemd:/run/systemd   \
-       -v /var/run/dbus:/var/run/dbus   \
-       -e container=docker \
        -e kube_cloud_provider=$CLOUD \
        -e k8s_provider=$K8S  \
        ksuderman/galaxy-k8s-boot:$VERSION
-
--- bin/runit.sh
+++ bin/runit.sh
@@ -9,16 +9,36 @@ DIR=/opt/galaxy-k8s-boot
 #VOLUMES="-v ./.kube:$DIR/.kube -v ./outputs:$DIR/outputs" # -v ./inventories:$DIR/inventories"
 VOLUMES=""
 
+while [[ $# > 0 ]] ; do
+  case $1 in
+    --gcp) CLOUD=gcp ;;
+    --aws) CLOUD=aws ;;
+    --openstack) CLOUD=openstack ;;
+    --k3s) K8S=k3s ;;
+    --rk3) K8S=rke ;;
+    --version) VERSION=$2 ; shift ;;
+    *)
+      echo "ERROR: Invalid option $1"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
 echo "Installing $CLOUD/$K8S $VERSION using $DIR"
 
-docker run -it --privileged $VOLUMES \
-       --network=host  --cgroupns=host  \
-       -v /etc/systemd/system:/etc/systemd/system   \
-       -v /sys/fs/cgroup:/sys/fs/cgroup:ro   \
-       -v /run/systemd:/run/systemd   \
-       -v /var/run/dbus:/var/run/dbus   \
-       -e container=docker \
+#docker run -it --privileged $VOLUMES \
+#       --network=host  --cgroupns=host  \
+#       -v /etc/systemd/system:/etc/systemd/system   \
+#       -v /sys/fs/cgroup:/sys/fs/cgroup:ro   \
+#       -v /run/systemd:/run/systemd   \
+#       -v /var/run/dbus:/var/run/dbus   \
+#       -e container=docker \
+#       -e kube_cloud_provider=$CLOUD \
+#       -e k8s_provider=$K8S  \
+#       ksuderman/galaxy-k8s-boot:$VERSION bash
+
+docker run --privileged \
        -e kube_cloud_provider=$CLOUD \
        -e k8s_provider=$K8S  \
        ksuderman/galaxy-k8s-boot:$VERSION bash
-
