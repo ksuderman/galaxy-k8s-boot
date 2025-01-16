d6ee2d9e5264863ea5eb409c2c677645ab519c2f
Keith Suderman <suderman@jhu.edu>
2025-01-16 11:59:41 -0600

    Parameterize cloud

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+cloud=aws
 provider=k3s
 if [[ $# > 0 ]] ; then
   case $1 in
     k3s|rke)
       provider=$1
+      ;;
+    aws|gcp|openstack)
+      cloud=$1
       ;;
     -h|--help)
       echo "USAGE: $(basename $0) [k3s|rke]"
@@ -16,5 +20,5 @@ if [[ $# > 0 ]] ; then
       ;;
   esac
 fi
-ansible-playbook -i inventories/hosts.ini -e kube_cloud_provider=openstack -e k8s_provider=$provider -c local playbook.yml
+ansible-playbook -i inventories/hosts.ini -e kube_cloud_provider=$cloud -e k8s_provider=$provider -c local playbook.yml
 
