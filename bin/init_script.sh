1138446ab999927aaa98f613a03620356c06d63a
Keith Suderman <suderman@jhu.edu>
2025-02-04 17:30:07 -0500

    Add template for localhost and set ansible_user and host IP

--- bin/init_script.sh
+++ bin/init_script.sh
@@ -22,6 +22,6 @@ sudo apt install -y ansible
 git clone https://github.com/galaxyproject/galaxy-k8s-boot.git
 cd galaxy-k8s-boot
 
-sed -i "s|extra_server_args=\"--tls-san localhost --disable traefik --v=4\"|extra_server_args=\"--tls-san $(curl -s http://169.254.169.254/latest/meta-data/public-ipv4) --disable traefik --v=4\"|" inventories/localhost
+cat inventories/localhost.template | sed "s/__HOST__/$(curl -s ifconfig.me)/" | sed "s/__USER__/$(whoami)/" > inventories/localhost
 
 ansible-playbook -i inventories/localhost playbook.yml --extra-vars "job_max_cores=$(($(nproc) - $RESERVED_CORES))" --extra-vars "job_max_mem=$(($(free -m | awk '/^Mem:/{print $2}') - $RESERVED_MEM_MB))" --extra-vars "application=$APPLICATION" --extra-vars "galaxy_api_key=$GALAXY_API_KEY" --extra-vars "pulsar_api_key=$PULSAR_API_KEY"
