fe328a3250004ff22efa17e920df7a32c5519805
Keith Suderman <suderman@jhu.edu>
2025-02-04 19:49:17 -0500

    The cloud init runs as root so we can't use whoami to get the ansible user name

--- bin/init_script.sh
+++ bin/init_script.sh
@@ -10,6 +10,7 @@ exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&
 APPLICATION=galaxy
 GALAXY_API_KEY=changeme
 PULSAR_API_KEY=changeme
+ANSIBLE_USER=azureuser
 #-------------------------------
 RESERVED_CORES=2
 RESERVED_MEM_MB=6144
@@ -22,6 +23,6 @@ sudo apt install -y ansible
 git clone https://github.com/galaxyproject/galaxy-k8s-boot.git
 cd galaxy-k8s-boot
 
-cat inventories/localhost.template | sed "s/__HOST__/$(curl -s ifconfig.me)/" | sed "s/__USER__/$(whoami)/" > inventories/localhost
+cat inventories/localhost.template | sed "s/__HOST__/$(curl -s ifconfig.me)/" | sed "s/__USER__/$ANSIBLE_USER/" > inventories/localhost
 
 ansible-playbook -i inventories/localhost playbook.yml --extra-vars "job_max_cores=$(($(nproc) - $RESERVED_CORES))" --extra-vars "job_max_mem=$(($(free -m | awk '/^Mem:/{print $2}') - $RESERVED_MEM_MB))" --extra-vars "application=$APPLICATION" --extra-vars "galaxy_api_key=$GALAXY_API_KEY" --extra-vars "pulsar_api_key=$PULSAR_API_KEY"
