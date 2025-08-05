4f26e1d150adf30da0da391dad8154d4befbdafd
Keith Suderman <suderman@jhu.edu>
2025-08-05 10:31:46 -0400

    Configuration to test metaphlan OOM errors on AWS

--- bin/aws.sh
+++ bin/aws.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
 if [[ $# -eq 0 ]] ; then
-  echo 'Usage: bin/aws.sh start|stop'
+  echo 'Usage: bin/aws.sh start|stop galaxy|pulsar'
   exit 1
 fi
 
-# Name of the instance to be created
-NAME=ks-dev
 # Name of the SSH key pair to use. This key pair must already exist in the AWS account.
 KEY=ks-galaxy-aws
 
@@ -18,16 +16,34 @@ IMAGE=ami-0e2c8caa4b6378d8c
 TYPE=m6i.4xlarge
 
 DIR=$(dirname $(realpath $0))
+NAME=ks-dev-galaxy
 
-command=$1
-shift
+command=
+app=
+#shift
 while [[ $# > 0 ]]; do
   case $1 in
+    start|stop) command=$1 ;;
+    galaxy|pulsar) app=$1 ;;
+    -c|--cores)
+      case $2 in
+        8) TYPE=m6i.2xlarge ;;
+        16) TYPE=m6i.4xlarge ;;
+        32) TYPE=m6i.8xlarge ;;
+        64) TYPE=m6i.16xlarge ;;
+        128) TYPE=m6i.32xlarge ;;
+        *)
+          echo "Invalid number of cores, must be one of 8, 16,32, 64, or 128"
+          exit
+          ;;
+      esac
+      shift
+      ;;
     -n|--name) NAME=$2 ; shift ;;
     -k|--key) KEY=$2 ; shift ;;
     -g|--group) GROUP=$2 ; shift ;;
     -i|--image) IMAGE=$2 ; shift ;;
-    -t|--type) SIZE=$2 ; shift ;;
+    -t|--type) TYPE=$2 ; shift ;;
     *)
       echo "Unknown option: $1"
       exit 1
@@ -35,6 +51,18 @@ while [[ $# > 0 ]]; do
   esac
   shift
 done
+
+if [[ -z $command ]] ; then
+  echo "Error: missing command."
+  echo 'Usage: bin/aws.sh [start|stop] [galaxy|pulsar]'
+  exit 1
+fi
+if [[ -z $app ]] ; then
+  echo "Error: missing app."
+  echo "Usage: bin/aws.sh $command [galaxy|pulsar]"
+  exit 1
+fi
+# The name of the instance to be created.
 
 mapping=$(cat << EOF
 [
@@ -56,9 +84,38 @@ mapping=$(cat << EOF
 EOF
 )
 
+function terminate() {
+    echo "Stopping $1..."
+    ID=$(aws ec2 describe-instances \
+            --filters "Name=tag:Name,Values=$1" \
+                      "Name=instance-state-name,Values=running" \
+            --query "Reservations[*].Instances[*].InstanceId" \
+            --output text)
+    if [[ -z $ID ]] ; then
+      echo "No running vm name $1 found."
+      return
+    fi
+    aws ec2 terminate-instances --instance-ids $ID
+
+}
+
 case $command in
   start)
     echo "Starting $NAME..."
+    case $app in
+      galaxy)
+        #TYPE=m6i.4xlarge
+        USER_DATA=file://$DIR/aws.yml
+        ;;
+      pulsar)
+        TYPE=m6i.4xlarge
+        USER_DATA=file://$DIR/pulsar.yml
+        ;;
+      *)
+        echo "Unknown app: $app"
+        exit 1
+        ;;
+    esac
     aws ec2 run-instances \
       --image-id $IMAGE \
       --instance-type $TYPE \
@@ -69,23 +126,13 @@ case $command in
       --block-device-mappings "$mapping" \
       --count 1 \
       --output json \
-      --user-data file://$DIR/aws.yml
+      --user-data $USER_DATA
     ;;
   stop)
-    echo "Stopping $NAME..."
-    ID=$(aws ec2 describe-instances \
-            --filters "Name=tag:Name,Values=$NAME" \
-                      "Name=instance-state-name,Values=running" \
-            --query "Reservations[*].Instances[*].InstanceId" \
-            --output text)
-    if [[ -z $ID ]] ; then
-      echo "No running vm found."
-      exit 1
-    fi
-    aws ec2 terminate-instances --instance-ids $ID
+    terminate $NAME
     ;;
   *)
-    echo 'Usage: bin/aws.sh start|stop'
+    echo 'Usage: bin/aws.sh start|stop galaxy|pulsar'
     exit 1
     ;;
 esac
