ba9928d61f1871136bb9b63f784c9b9603f7f344
Keith Suderman <suderman@jhu.edu>
2026-01-08 09:51:01 -0500

    Remove duplicate test toolshed config

--- bin/restart.sh
+++ bin/restart.sh
@@ -12,6 +12,5 @@ bin/launch_vm.sh $SERVER \
   --disk-size 256 \
   --reuse-existing-data \
   -f values/hybrid-gcp-batch.yml \
-  -f values/test-gcp-batch-comparison.yml \
-  -f values/test-toolshed.yml
-  
+  -f values/test-gcp-batch-comparison.yml 
+ 
--- bin/start.sh
+++ bin/start.sh
@@ -11,5 +11,4 @@ bin/launch_vm.sh $SERVER \
   --git-branch $BRANCH \
   --disk-size 256 \
   -f values/hybrid-gcp-batch.yml \
-  -f values/test-gcp-batch-comparison.yml \
-  -f values/test-toolshed.yml
+  -f values/test-gcp-batch-comparison.yml 
