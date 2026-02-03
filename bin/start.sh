020aaa02d386dae6ad585fb14c76cd08f7f4a113
Keith Suderman <suderman@jhu.edu>
2026-02-03 14:16:39 -0500

    Trying a gcp batch enabled deployment

--- bin/start.sh
+++ bin/start.sh
@@ -13,7 +13,8 @@ echo "Launching ${SERVER} with hybrid GCP Batch configuration"
   --disk-size 256 \
   -f values/values.yml \
   -f values/wait.yml \
-  -f values/guerler.yml 
-#  -f values/batch.yml \
-#  -f values/rules.yml \
-#  -f values/resource-params.yml
+  -f values/batch.yml \
+  -f values/rules.yml \
+  -f values/resource-params.yml \
+  -f values/v25.1-batch.yml
+
