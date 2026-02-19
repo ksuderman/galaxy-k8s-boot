176a3870c6016ec2d143c4cc9c5fc54e9af9dca7
Keith Suderman <suderman@jhu.edu>
2026-02-19 12:04:36 -0500

    Don't delete Batch jobs

--- bin/start.sh
+++ bin/start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 #BRANCH=41-fix-nfs
-BRANCH=${BRANCH:-persistent-data-merged}
-SERVER=${SERVER:-ks-dev-batch}
+BRANCH=${BRANCH:-new-gcp-batch-runner-testing}
+SERVER=${SERVER:-ks-batch-test}
 REPO=${REPO:-https://github.com/ksuderman/galaxy-k8s-boot}
 
 echo "Launching ${SERVER}"
@@ -10,8 +10,13 @@ echo "Launching ${SERVER}"
 bin/launch_vm.sh $SERVER \
   --git-repo $REPO \
   --git-branch $BRANCH \
-  --disk-size 256
+  --disk-size 256 \
+  -f values/values.yml \
+  -f values/batch.yml \
+  -f values/rules.yml \
+  -f values/resource-params.yml \
+  -f values/v26.0.yml
 
+#  -f values/wait.yml \
+#  -f values/v25.1-batch.yml
 
-#--enable-gcp-batch \
-#-f values/values.yml -f values/batch.yml -f values/v25.1-auto.yml -f values/rules.yml -f values/resource-params.yml -f values/probes.yml
